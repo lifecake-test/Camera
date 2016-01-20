aae6decb9d70060435a4183fd2e1b007e22fefab
artjom <artjom@lifecake.com>
2016-01-20 10:45:17 +0000

    camera tests

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,17 +1,52 @@
 package com.develogical.camera;
 
-public class Camera {
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Camera implements WriteListener {
+
+    private final Sensor sensor;
+    private final MemoryCard memCard;
+
+    private Boolean isOff = true;
+    private Boolean isWriting = false;
+    private Boolean isPendingPowerOff = false;
+
+    public Camera(Sensor sensor, MemoryCard memCard) {
+        this.sensor = sensor;
+        this.memCard = memCard;
+    }
 
     public void pressShutter() {
-        // not implemented
+        if (!this.isOff) {
+            byte[] data = this.sensor.readData();
+            this.isWriting = true;
+            this.memCard.write(data);
+        }
     }
 
     public void powerOn() {
-        // not implemented
+        this.sensor.powerUp();
+        this.isOff = false;
     }
 
     public void powerOff() {
-       // not implemented
+        this.isOff = true;
+
+        if (!this.isWriting) {
+            this.sensor.powerDown();
+        } else {
+            this.isPendingPowerOff = true;
+        }
+    }
+
+    @Override
+    public void writeComplete() {
+        this.isWriting = false;
+
+        if (this.isPendingPowerOff) {
+            this.isPendingPowerOff = false;
+            powerOff();
+        }
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,18 +1,86 @@
 package com.develogical.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.jmock.internal.Cardinality.exactly;
 
 public class CameraTest {
 
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    Sensor sensor = context.mock(Sensor.class);
+    MemoryCard memCard = context.mock(MemoryCard.class);
+
+    Camera camera = new Camera(sensor, memCard);
+
+    private void powerOnCameraAndPressShutter() {
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerUp();
+
+            byte[] data = exactly(1).of(sensor).readData();
+            exactly(1).of(memCard).write(data);
+        }});
+
+        camera.powerOn();
+        camera.pressShutter();
+    }
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
 
-         // write your test here
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerUp();
+        }});
+
+        camera.powerOn();
     }
 
+    @Test
+    public void switchingTheCameraOffPowersUpTheSensor() {
+
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerDown();
+        }});
+
+        camera.powerOff();
+    }
+
+    @Test
+    public void pressingTheShutterWhileOffDoesNothing() {
+
+        context.checking(new Expectations() {{
+            never(sensor).readData();
+        }});
+
+        camera.pressShutter();
+    }
+
+    @Test
+    public void pressingTheShutterWhileOnCopiesData() {
+        powerOnCameraAndPressShutter();
+    }
+
+    @Test
+    public void switchingCameraOffDoesNotPowerOffBusySensor() {
+        powerOnCameraAndPressShutter();
+        camera.powerOff();
+    }
+
+
+    @Test
+    public void switchingCameraOffPowersOffBusySensorAfterWriteCompleted() {
+
+        powerOnCameraAndPressShutter();
+
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerDown();
+        }});
+
+        camera.powerOff();
+        camera.writeComplete();
+    }
 }
